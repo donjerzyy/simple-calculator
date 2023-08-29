362d1b56596c8917814be83e6a2fa53f2d17d99f
Donald Ray <donjerzy46@gmail.com>
2023-08-29 22:46:55 +0300

    Add calculator GUI

--- javascript.js
+++ javascript.js
@@ -15,3 +15,22 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(num1, num2, operator) {
+
+    if(operator === 'add') {
+        return add(num1, num2);
+    } else if(operator === 'sub') {
+        return subtract(num1, num2);
+    } else if(operator === 'mul') {
+        return multiply(num1, num2);
+    } else {
+        return divide(num1, num2)
+    }
+
+}
+
+
+let firstNumber;
+let secondNumber;
+let operator;
