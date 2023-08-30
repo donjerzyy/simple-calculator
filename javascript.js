71cea0c71ac38c882c82dc8faa7ae63e933cf31e
Donald Ray <donjerzy46@gmail.com>
2023-08-30 09:42:22 +0300

    Add show button clicked on display

--- javascript.js
+++ javascript.js
@@ -1,3 +1,29 @@
+let displayValue;
+let firstNumber;
+let secondNumber;
+let operator;
+let displayScreen = document.querySelector('.display');
+let ac = document.querySelector('.ac');
+let nine = document.querySelector('.nine');
+let eight = document.querySelector('.eight');
+let seven = document.querySelector('.seven');
+let six = document.querySelector('.six');
+let five = document.querySelector('.five');
+let four = document.querySelector('.four');
+let three = document.querySelector('.three');
+let two = document.querySelector('.two');
+let one = document.querySelector('.one');
+let zero = document.querySelector('.zero');
+let equals = document.querySelector('.equals');
+let dot = document.querySelector('.dot');
+let addButton = document.querySelector('.operations-add');
+let subButton = document.querySelector('.operations-sub');
+let divButton = document.querySelector('.operations-div');
+let mulButton = document.querySelector('.operations-mul');
+
+
+
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -30,7 +56,45 @@ function operate(num1, num2, operator) {
 
 }
 
+function clearDisplay() {
+    displayScreen.textContent = "";
+}
 
-let firstNumber;
-let secondNumber;
-let operator;
+function updateDisplay (value) {
+    if(value === 'x') {
+        let currentValue = displayScreen.textContent;
+        displayScreen.innerHTML = `${currentValue}&times;`
+    } else {
+        let currentValue = displayScreen.textContent;
+        currentValue += value;
+        displayScreen.textContent = currentValue;
+    }
+    
+}
+
+
+
+
+ac.addEventListener('mousedown', () => clearDisplay());
+nine.addEventListener('mousedown', ()=> updateDisplay(9));
+eight.addEventListener('mousedown', ()=> updateDisplay(8));
+seven.addEventListener('mousedown', ()=> updateDisplay(7));
+six.addEventListener('mousedown', ()=> updateDisplay(6));
+five.addEventListener('mousedown', ()=> updateDisplay(5));
+four.addEventListener('mousedown', ()=> updateDisplay(4));
+three.addEventListener('mousedown', ()=> updateDisplay(3));
+two.addEventListener('mousedown', ()=> updateDisplay(2));
+one.addEventListener('mousedown', ()=> updateDisplay(1));
+zero.addEventListener('mousedown', ()=> updateDisplay(0));
+dot.addEventListener('mousedown', ()=> updateDisplay('.'));
+addButton.addEventListener('mousedown', ()=> updateDisplay('+'));
+subButton.addEventListener('mousedown', ()=>updateDisplay('-'));
+divButton.addEventListener('mousedown', ()=>updateDisplay('÷'));
+mulButton.addEventListener('mousedown', ()=>updateDisplay('x'));
+
+
+
+
+
+
+
